c6b77ed3cf4c3f29e0079603c372104032adf067
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-08-23 13:51:59 -0500

    [+] Pages > *: 1. Display one menu item per page to the main menu 2. Update the view of each page by adding stylesheets 3. Eliminate default implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,37 +13,26 @@ export class AppComponent implements OnInit {
   public selectedIndex = 0;
   public appPages = [
     {
-      title: 'Inbox',
-      url: '/folder/Inbox',
+      title: 'Home',
+      url: '/home',
+      icon: 'home'
+    },
+    {
+      title: 'About Us',
+      url: '/about',
+      icon: 'information-circle'
+    },
+    {
+      title: 'Menu',
+      url: '/menu',
+      icon: 'list'
+    },
+    {
+      title: 'Contact Us',
+      url: '/contact',
       icon: 'mail'
     },
-    {
-      title: 'Outbox',
-      url: '/folder/Outbox',
-      icon: 'paper-plane'
-    },
-    {
-      title: 'Favorites',
-      url: '/folder/Favorites',
-      icon: 'heart'
-    },
-    {
-      title: 'Archived',
-      url: '/folder/Archived',
-      icon: 'archive'
-    },
-    {
-      title: 'Trash',
-      url: '/folder/Trash',
-      icon: 'trash'
-    },
-    {
-      title: 'Spam',
-      url: '/folder/Spam',
-      icon: 'warning'
-    }
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
 
   constructor(
     private platform: Platform,
@@ -61,7 +50,8 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
-    const path = window.location.pathname.split('folder/')[1];
+    const path = window.location.pathname;
+    console .log( 'path', path );
     if (path !== undefined) {
       this.selectedIndex = this.appPages.findIndex(page => page.title.toLowerCase() === path.toLowerCase());
     }
