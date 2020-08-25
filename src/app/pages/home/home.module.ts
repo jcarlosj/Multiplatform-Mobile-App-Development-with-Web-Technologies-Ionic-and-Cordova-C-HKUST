7c443033913ce771b21fd67c302d1da03f19cf22
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-08-25 13:38:35 -0500

    [+] Page > Home: 1. Implement Services and Modules to the Home Component Module 2. Consume Services to display Dish, Promotion and Leader 3. Displays Dish, Promotion and Leader featured in the Component

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,20 +1,43 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { IonicModule } from '@ionic/angular';
 
+/** Services */
+import { DishService } from '../../services/dish.service';
+import { PromotionService } from '../../services/promotion.service';
+import { LeaderService } from '../../services/leader.service';
+import { ProcessHttpMessageService } from '../../services/process-http-message.service';
+
+/** Router */
 import { HomePageRoutingModule } from './home-routing.module';
 
+/** Page Components */
 import { HomePage } from './home.page';
+
+/** Base URL - BackEnd Server (It is a recommended practice) */
+import { BASE_URL } from '../../shared/baseurl';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    HttpClientModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
+  providers: [
+    DishService,
+    PromotionService,
+    LeaderService,
+    ProcessHttpMessageService,
+    {
+        provide: 'BaseURL',
+        useValue: BASE_URL
+    }
+  ]
 })
 export class HomePageModule {}
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+
+/** Models */
+import { Dish } from '../../shared/interfaces/Dish';
+import { Promotion } from '../../shared/interfaces/Promotion';
+import { Leader } from '../../shared/interfaces/Leader';
+
+/** Services */
+import { DishService } from '../../services/dish.service';
+import { PromotionService } from '../../services/promotion.service';
+import { LeaderService } from '../../services/leader.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +16,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+    /** Atributes */
+    dish: Dish;
+    dishErrorMessage: string;
+    promotion: Promotion;
+    promotionErrorMessage: string;
+    leader: Leader;
+    leaderErrorMessage: string;
 
-  constructor() { }
+    constructor(
+        private dishService: DishService,
+        private promotionService: PromotionService,
+        private leaderService: LeaderService,
+        @Inject( 'BaseURL' ) public BaseURL            // Mechanism for letting Angular know that a parameter must be injected
+    ) {
+        console .log( 'BaseURL', this .BaseURL );
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        /** Receive a Observable */
+        this .dishService .getFeaturedDish()
+             .subscribe(    // Subscription to the Observable receives two callbacks, the data obtained, the error messages
+                  dish => this .dish = dish,
+                  error => this .dishErrorMessage = <any> error
+              );
+        /** Receive a Observable */
+        this .promotionService .getFeaturedPromotion()
+             .subscribe(    // Subscription to the Observable receives two callbacks, the data obtained, the error messages
+                  promotion => this .promotion = promotion,
+                  error => this  .promotionErrorMessage = <any> error
+              );
+        /** Receive a Observable */
+        this .leaderService .getFeaturedLeader()
+             .subscribe(    // Subscription to the Observable receives two callbacks, the data obtained, the error messages
+                  leader => this .leader = leader,
+                  error => this .leaderErrorMessage = <any> error
+              );
+    }
 
 }
