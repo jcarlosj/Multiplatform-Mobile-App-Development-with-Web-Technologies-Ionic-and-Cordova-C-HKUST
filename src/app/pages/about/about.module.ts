763583945447f5f353a6538cd8c3fd197e4d56ea
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-08-31 13:15:30 -0500

    [+] Task 2 y 3: 1. Show history of the restaurant in the About Component 2. Use the Leaders Service to obtain data 3. Display data obtained in an Ionic List Component

--- src/app/pages/about/about.module.ts
+++ src/app/pages/about/about.module.ts
@@ -1,20 +1,39 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { IonicModule } from '@ionic/angular';
 
+/** Services */
+import { LeaderService } from '../../services/leader.service';
+import { ProcessHttpMessageService } from '../../services/process-http-message.service';
+
+/** Router */
 import { AboutPageRoutingModule } from './about-routing.module';
 
+/** Page Components */
 import { AboutPage } from './about.page';
+
+/** Base URL - BackEnd Server (It is a recommended practice) */
+import { BASE_URL } from '../../shared/baseurl';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    HttpClientModule,
     AboutPageRoutingModule
   ],
-  declarations: [AboutPage]
+  declarations: [AboutPage],
+  providers: [
+    LeaderService,
+    ProcessHttpMessageService,
+    {
+        provide: 'BaseURL',
+        useValue: BASE_URL
+    }
+  ]
 })
 export class AboutPageModule {}
--- src/app/pages/about/about.page.ts
+++ src/app/pages/about/about.page.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+
+/** Models */
+import { Leader } from '../../shared/interfaces/Leader';
+
+/** Services */
+import { LeaderService } from '../../services/leader.service';
 
 @Component({
   selector: 'app-about',
@@ -6,10 +12,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.page.scss'],
 })
 export class AboutPage implements OnInit {
+    /** Atributes */
+    leaders: Leader[];
+    leadersErrorMessage: string;
 
-  constructor() { }
+    constructor(
+        private leaderService: LeaderService,
+        @Inject( 'BaseURL' ) public BaseURL            // Mechanism for letting Angular know that a parameter must be injected
+    ) { 
+        console .log( 'BaseURL', this .BaseURL );
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        /** Receive a Observable */
+        this .leaderService .getLeaders()
+              .subscribe(
+                  leaders => this .leaders = leaders,
+                  error => this . leadersErrorMessage = <any>error
+              );
+    }
 
 }
