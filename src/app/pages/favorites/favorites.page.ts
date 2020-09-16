54d8d0e89d8cd524ec8786421210d72b040a65ee
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-09-16 12:29:00 -0500

    [+] Page > Favorites: Showing an alert message to confirm the removal of a dish from the favorites list

--- src/app/pages/favorites/favorites.page.ts
+++ src/app/pages/favorites/favorites.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { ToastController, LoadingController  } from '@ionic/angular';
+import { ToastController, LoadingController, AlertController } from '@ionic/angular';
 
 /** Models */
 import { Dish } from '../../shared/interfaces/Dish';
@@ -23,7 +23,8 @@ export class FavoritesPage implements OnInit {
         private router: Router,
         private favoriteService: FavoriteService,
         public toastController: ToastController,   
-        public loadingController: LoadingController,     
+        public loadingController: LoadingController,  
+        public alertController: AlertController,   
         @Inject( 'BaseURL' ) private BaseURL
     ) { 
         console .log( 'BaseURL', this .BaseURL );
@@ -44,34 +45,60 @@ export class FavoritesPage implements OnInit {
     async deleteFavorite( id: number ) {
         console .log( 'deleteFavorite', id );
 
-        /** Define Loading */
-        const loading = await this .loadingController .create({
-            cssClass: 'ion-loading-delete-favorite',
-            message: 'Deleting...',
-            duration: 2000
+        /** Define Alert */
+        const alert = await this .alertController .create({
+            cssClass: 'ion-alert-delete-favorite',
+            header: 'Confirm Delete!',
+            message: `Do you want to <strong>delete</strong> Dish ${ id }?`,
+            buttons: [
+                {
+                    text: 'Cancel',
+                    role: 'cancel',
+                    cssClass: 'secondary',
+                    handler: ( blah ) => {
+                        console .log( 'Delete Cancelled!' );
+                    }
+                }, 
+                {
+                    text: 'Ok, Delete',
+                    handler: async () => {
+                        console .log( 'Confirm Okay' );
+
+                        /** Define Loading */
+                        const loading = await this .loadingController .create({
+                            cssClass: 'ion-loading-delete-favorite',
+                            message: 'Deleting...',
+                            duration: 2000
+                        });
+
+                        await loading .present();
+
+                        /** Define Toast */
+                        const toast = await this .toastController .create({
+                            message: `Dish ${ id } deleted successfully`,
+                            duration: 3000
+                        });
+
+                        /** Using the Service to remove a dish from the favorites list */
+                        this .favoriteService .deleteFavorite( id )
+                                .subscribe( 
+                                    favorites => {
+                                        this .favorites = favorites;
+                                        loading .dismiss();
+                                        toast .present();
+                                    },
+                                    error => {
+                                        this .errorMessage = error;
+                                        loading .dismiss();
+                                    } 
+                                );
+
+                    }   // handler
+                }
+            ]
         });
-
-        await loading .present();
-
-        /** Define Toast */
-        const toast = await this .toastController .create({
-            message: `Dish ${ id } deleted successfully`,
-            duration: 3000
-        });
-
-        /** Using the Service to remove a dish from the favorites list */
-        this .favoriteService .deleteFavorite( id )
-                .subscribe( 
-                    favorites => {
-                        this .favorites = favorites;
-                        loading .dismiss();
-                        toast .present();
-                    },
-                    error => {
-                        this .errorMessage = error;
-                        loading .dismiss();
-                    } 
-                );
+      
+        await alert .present();
 
     }
 
