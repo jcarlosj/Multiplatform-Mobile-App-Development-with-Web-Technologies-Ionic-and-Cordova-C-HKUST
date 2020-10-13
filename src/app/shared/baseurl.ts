cded8b5f10b62035b9931983f267924cbfba0e7f
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2020-10-13 09:58:36 -0500

    [+] Project: 1. Install cordova globally 2. Build the Project 3. Assign ID to the application (ionic.config.json) 4. Change BASE_URL to local IP (baseurl.ts) 5. Remove tracking to the generated directory resources (.gitignore)

--- src/app/shared/baseurl.ts
+++ src/app/shared/baseurl.ts
@@ -1,1 +1,2 @@
-export const BASE_URL = 'http://localhost:5000/';
+// export const BASE_URL = 'http://localhost:5000/';
+export const BASE_URL = 'http://192.168.1.107:5000/';       // Updating localhost to the IP address of your computer
